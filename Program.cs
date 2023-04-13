e3688ccdd2add3c4e2843faa9eeafda4c8ef46a9
Bruno Barreto <106797767+TheKods@users.noreply.github.com>
2023-04-13 16:23:05 -0300

    First code C#
    First code C#

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace FirstProject {
+    internal class Program {
+        static void Main(string[] args) {
+
+            byte x = 127;
+            int y = 1000;
+            int z = 2147483647;
+            long a = 2147483648L;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(a);
+        }
+    }
+}
