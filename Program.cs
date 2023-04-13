ca89fe78a3fcd9b5f009d7d00805d94dd85ec47a
Bruno Barreto <106797767+TheKods@users.noreply.github.com>
2023-04-13 17:00:22 -0300

    Complementing data types
    Complementing data types

--- Program.cs
+++ Program.cs
@@ -4,15 +4,33 @@ namespace FirstProject {
     internal class Program {
         static void Main(string[] args) {
 
+
+            bool completo = false;
+            char genero = 'F';
+            char letra = '\u0041';
             byte x = 127;
             int y = 1000;
             int z = 2147483647;
             long a = 2147483648L;
+            float b = 4.5f;
+            double c = 4.6;
+            string nome = "Bruno Oliveira";
+            object obj1 = "Teste";
+            object obj2 = 4.5f;
+            
 
+            Console.WriteLine(completo);
+            Console.WriteLine(genero);
+            Console.WriteLine(letra);
             Console.WriteLine(x);
             Console.WriteLine(y);
             Console.WriteLine(z);
             Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(nome);
+            Console.WriteLine(obj1);
+            Console.WriteLine(obj2);
         }
     }
 }
