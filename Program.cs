09f76f953585e6a5e8b4554c26ebecace83e3e2d
Bruno Barreto <106797767+TheKods@users.noreply.github.com>
2023-04-18 15:45:23 -0300

    Min_Maxvalue
    Estudo sobre minimo e maximo para tipo de variaveis.

--- Program.cs
+++ Program.cs
@@ -4,33 +4,16 @@ namespace FirstProject {
     internal class Program {
         static void Main(string[] args) {
 
+            int n1 = int.MinValue;
+            int n2 = int.MaxValue;
+            sbyte n3= sbyte.MinValue;
+            decimal n4= decimal.MaxValue;
 
-            bool completo = false;
-            char genero = 'F';
-            char letra = '\u0041';
-            byte x = 127;
-            int y = 1000;
-            int z = 2147483647;
-            long a = 2147483648L;
-            float b = 4.5f;
-            double c = 4.6;
-            string nome = "Bruno Oliveira";
-            object obj1 = "Teste";
-            object obj2 = 4.5f;
-            
 
-            Console.WriteLine(completo);
-            Console.WriteLine(genero);
-            Console.WriteLine(letra);
-            Console.WriteLine(x);
-            Console.WriteLine(y);
-            Console.WriteLine(z);
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-            Console.WriteLine(c);
-            Console.WriteLine(nome);
-            Console.WriteLine(obj1);
-            Console.WriteLine(obj2);
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
         }
     }
 }
